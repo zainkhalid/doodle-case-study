1b334390f5d909ea83b4a67a9cb0a93ff1643212
Noel Welsh <noel@underscore.io>
2015-09-02 12:03:23 +0100

    Fix key handling and resource usage
    Java canvas now captures key presses---it must enable focus for key
    events to be passed on to it.
    
    Clean up resources when we close the window.

--- jvm/src/main/scala/doodle/jvm/CanvasFrame.scala
+++ jvm/src/main/scala/doodle/jvm/CanvasFrame.scala
@@ -2,10 +2,24 @@ package doodle
 package jvm
 
 import javax.swing.JFrame
+import java.awt.event.{WindowListener, WindowEvent}
 
 class CanvasFrame extends JFrame {
   val panel = new CanvasPanel()
 
   getContentPane().add(panel)
+  panel.setFocusable(true)
   pack()
+
+  this.addWindowListener(new WindowListener {
+                           def windowClosed(e: WindowEvent): Unit =
+                             panel.close()
+                           def windowClosing(e: WindowEvent): Unit =
+                             panel.close()
+                           def windowActivated(e: WindowEvent): Unit = ()
+                           def windowDeactivated(e: WindowEvent): Unit = ()
+                           def windowDeiconified(e: WindowEvent): Unit = ()
+                           def windowIconified(e: WindowEvent): Unit = ()
+                           def windowOpened(e: WindowEvent): Unit = ()
+                         })
 }
